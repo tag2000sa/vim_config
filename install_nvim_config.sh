06283508399a6cfe749a5e7a9c723d27267e3067
TurkyGary <tag2000sa@users.noreply.github.com>
2022-09-03 18:35:32 +0300

    Update install_nvim_config.sh
    imporve the script execution

--- install_nvim_config.sh
+++ install_nvim_config.sh
@@ -2,8 +2,8 @@
 #Author: Eng. Turky AbdulAziz Abdulhafiz
 #Create Date: 23/09/2021 3:45 PM
 
-mkdir -p ~/.config/nvim >> /dev/null &
-mkdir -p ~/.config/vim >> /dev/null &
-cp init.vim ~/.config/nvim >> /dev/null &
-cp -r templates ~/.config/vim >> /dev/null &
+mkdir -p ~/.config/nvim >> /dev/null &&
+mkdir -p ~/.config/vim >> /dev/null &&
+cp init.vim -t ~/.config/nvim >> /dev/null &&
+cp -r templates -t ~/.config/vim >> /dev/null &&
 echo "nvim_config installed, have fun ;)"

a70e58d785a3942fd28afa5bf0c94a80214524dd
TurkyGary <tag2000sa@users.noreply.github.com>
2022-09-03 18:37:09 +0300

    Update install_vim_config.sh
    improve the script

--- install_vim_config.sh
+++ install_vim_config.sh
@@ -2,7 +2,7 @@
 #Author: Eng. Turky AbdulAziz Abdulhafiz
 #Create Date: 23/09/2021 3:45 PM
 
-mkdir -p ~/.config/vim >> /dev/null &
-cp init.vim ~/.vimrc >> /dev/null &
-cp -r templates ~/.config/vim >> /dev/null &
+mkdir -p ~/.config/vim >> /dev/null &&
+cp -T init.vim ~/.vimrc >> /dev/null &&
+cp -r templates -t ~/.config/vim >> /dev/null &&
 echo "vim_config installed, have fun ;)"
